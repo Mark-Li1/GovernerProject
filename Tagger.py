af5cc8a5be95844da9ea25a401746e15008a0b89
Mark-Li1 <109117103+Mark-Li1@users.noreply.github.com>
2023-03-16 22:14:56 -0400

    Update Tagger.py
    Added the CSV write

--- Tagger.py
+++ Tagger.py
@@ -1,11 +1,10 @@
 # Given an input in Cleaned.txt, this code tags each word as a verb, pronoun, or some other type, as well as
 # breaking it up.
 import nltk as nk
+import csv
 
 Unprocessed = open("Cleaned.txt", "r")
 Query = Unprocessed.readline()
-
-
 
 tokens = nk.word_tokenize(Query)
 print(tokens)
@@ -14,3 +13,9 @@ nk.download('maxent_ne_chunker')
 nk.download('words')
 tagged = nk.pos_tag(tokens)
 print(tagged)
+# Once tagged, start sentiment analysis.
+with open("Tagged.csv", "w",newline='') as out:
+    csv_out = csv.writer(out)
+    csv_out.writerow(["word", "type"])
+    for row in tagged:
+        csv_out.writerow(row)
